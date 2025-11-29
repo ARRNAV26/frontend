3ae3d3a975375fc916017e0a2942db0146574b3b
arnavsaha26 <arnav@rhythmflows.com>
2025-11-29 22:43:28 +0530

    done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import { API_CONFIG, API_ENVIRONMENT, isProduction } from "../config/apiConfig";
+import { API_CONFIG } from "../config/apiConfig";
 import {
   Container,
   Typography,
